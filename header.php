f7080f7afc0f29de6cc9a0e450b1ada5f9ece192
Mark Endley <markendley@gmail.com>
2021-01-14 10:27:39 +1100

    hotjar tracking code

--- header.php
+++ header.php
@@ -79,7 +79,17 @@
 		}
 	?>
 	<body <?php body_class($post_slug); ?>>
-
+		<!-- Hotjar Tracking Code for https://flowmountainbike.com/ -->
+			<script>
+			(function(h,o,t,j,a,r){
+				h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+				h._hjSettings={hjid:2184315,hjsv:6};
+				a=o.getElementsByTagName('head')[0];
+				r=o.createElement('script');r.async=1;
+				r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+				a.appendChild(r);
+			})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
 
 		<header class="main-header">
 
